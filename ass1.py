e6613e5fa77799fa743b48af670ab4d424384ded
sumini0516 <sumini0516@catholic.ac.kr>
2022-11-24 16:31:33 +0900

    1124_1

--- ass1.py
+++ ass1.py
@@ -1,16 +1,132 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+assert sys.version_info[0] == 3
+assert sys.version_info[1] >= 5
+
+from gensim.models import KeyedVectors
+from gensim.test.utils import datapath
+import pprint
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = [10, 5]
+import nltk
+
+nltk.download('reuters')
+from nltk.corpus import reuters
+import numpy as np
+import random
+import scipy as sp
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA
+
+START_TOKEN = '<START>'
+END_TOKEN = '<END>'
+
+np.random.seed(0)
+random.seed(0)
+
+'''
+Word Vectors are often used as a fundamental component for downstream NLP tasks, e.g. question answering, text generation, 
+translation, etc., so it is important to build some intuitions as to their strengths and weaknesses. Here, you will explore two types of word vectors: those derived from *co-occurrence matrices*, and those derived via *GloVe*. 
+
+**Note on Terminology:** The terms "word vectors" and "word embeddings" are often used interchangeably. 
+The term "embedding" refers to the fact that we are encoding aspects of a word's meaning in a lower dimensional space. 
+As [Wikipedia](https://en.wikipedia.org/wiki/Word_embedding) states, "*conceptually it involves a mathematical embedding from a space with one dimension per word to a continuous vector space with a much lower dimension*".
+'''
+
+'''
+Many word vector implementations are driven by the idea that similar words, i.e., (near) synonyms, will be used in similar contexts. 
+As a result, similar words will often be spoken or written along with a shared subset of words, i.e., contexts. 
+By examining these contexts, we can try to develop embeddings for our words. 
+With this intuition in mind, many “old school” approaches to constructing word vectors relied on word counts. 
+Here we elaborate upon one of those strategies, co-occurrence matrices.
+'''
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def read_corpus(category="crude"):
+    files = reuters.fileids(category)
+    return [[START_TOKEN] + [w.lower() for w in list(reuters.words(f))] + [END_TOKEN] for f in files]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def distinct_words(corpus):
+    """ Determine a list of distinct words for the corpus.
+            Params:
+                corpus (list of list of strings): corpus of documents
+            Return:
+                corpus_words (list of strings): sorted list of distinct words across the corpus
+                num_corpus_words (integer): number of distinct words across the corpus
+    """
+    corpus_words = []
+    num_corpus_words = -1
+    for sentence in corpus:
+        for word in sentence:
+            if word not in corpus_words:
+                corpus_words.append(word)
+            else:
+                continue
+    corpus_words = sorted(corpus_words)
+    num_corpus_words = len(corpus_words)
+    return corpus_words, num_corpus_words
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def compute_co_occurrence_matrix(corpus, window_size=4):
+    """ Compute co-occurrence matrix for the given corpus and window_size (default of 4).
+
+        Note: Each word in a document should be at the center of a window. Words near edges will have a smaller
+              number of co-occurring words.
+
+              For example, if we take the document "<START> All that glitters is not gold <END>" with window size of 4,
+              "All" will co-occur with "<START>", "that", "glitters", "is", and "not".
+
+        Params:
+            corpus (list of list of strings): corpus of documents
+            window_size (int): size of context window
+        Return:
+            M (a symmetric numpy matrix of shape (number of unique words in the corpus , number of unique words in the corpus)):
+                Co-occurence matrix of word counts.
+                The ordering of the words in the rows/columns should be the same as the ordering of the words given by the distinct_words function.
+            word2ind (dict): dictionary that maps word to index (i.e. row/column number) for matrix M.
+    """
+    words, num_words = distinct_words(corpus)
+    M = None
+    word2ind = {}
+
+    # ------------------
+    # Write your implementation here.
+
+    # ------------------
+
+    return M, word2ind
+
+# Question 1.1
+reuters_corpus = read_corpus()
+# pprint.pprint(reuters_corpus[:3], compact=True, width=100)
+
+# Define toy corpus
+test_corpus = ["{} All that glitters isn't gold {}".format(START_TOKEN, END_TOKEN).split(" "),
+               "{} All's well that ends well {}".format(START_TOKEN, END_TOKEN).split(" ")]
+test_corpus_words, num_corpus_words = distinct_words(test_corpus)
+print(test_corpus_words, num_corpus_words)
+
+'''
+# Correct answers
+ans_test_corpus_words = sorted([START_TOKEN, "All", "ends", "that", "gold", "All's", "glitters", "isn't", "well", END_TOKEN])
+ans_num_corpus_words = len(ans_test_corpus_words)
+
+# Test correct number of words
+assert(num_corpus_words == ans_num_corpus_words), "Incorrect number of distinct words. Correct: {}. Yours: {}".format(ans_num_corpus_words, num_corpus_words)
+
+# Test correct words
+assert (test_corpus_words == ans_test_corpus_words), "Incorrect corpus_words.\nCorrect: {}\nYours:   {}".format(str(ans_test_corpus_words), str(test_corpus_words))
+
+# Print Success
+print ("-" * 80)
+print("Passed All Tests!")
+print ("-" * 80)
+'''
+
+#Question 1.2
+# Define toy corpus and get student's co-occurrence matrix
+test_corpus = ["{} All that glitters isn't gold {}".format(START_TOKEN, END_TOKEN).split(" "), "{} All's well that ends well {}".format(START_TOKEN, END_TOKEN).split(" ")]
+M_test, word2ind_test = compute_co_occurrence_matrix(test_corpus, window_size=1)
+
