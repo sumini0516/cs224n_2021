10537ada0e4d1fddb92597a98575e1135f3eebcf
sumini0516 <sumini0516@catholic.ac.kr>
2022-11-29 18:59:35 +0900

    1129_3

--- test.py
+++ test.py
@@ -15,7 +15,3 @@ M_test_ans = np.array(
 ans_test_corpus_words = sorted(["START_TOKEN", "All", "ends", "that", "gold", "All's", "glitters", "isn't", "well", "END_TOKEN"])
 word2ind_ans = dict(zip(ans_test_corpus_words, range(len(ans_test_corpus_words))))
 print(word2ind_ans)
-
-if right_idx < corpus_size:
-                right_word_id = corpus[right_idx]
-                co_matrix[word_id, right_word_id] += 1
